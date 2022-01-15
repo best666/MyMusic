79889048fa1ede5f724f160f2838e6bebfeaff2d
王超 <754393889@qq.com>
2022-01-15 16:26:57 +0800

    引入vant，首页状态管理方案

--- service/api_video.js
+++ service/api_video.js
@@ -6,3 +6,29 @@ export function getTopMV(offset, limit = 10) {
     limit
   })
 }
+
+/**
+ * 请求MV的播放地址
+ * @param {number} id MV的id
+ */
+export function getMVURL(id) {
+  return hyRequest.get("/mv/url", {
+    id
+  })
+}
+
+/**
+ * 请求MV的详情
+ * @param {number} mvid MV的id
+ */
+export function getMVDetail(mvid) {
+  return hyRequest.get("/mv/detail", {
+    mvid
+  })
+}
+
+export function getRelatedVideo(id) {
+  return hyRequest.get("/related/allvideo", {
+    id
+  })
+}
