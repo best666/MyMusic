efe4175d513474b6fed6ff75239140bd12e5bd4d
王超 <754393889@qq.com>
2022-01-21 14:39:30 +0800

    视频详情页、音乐首页、播放

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,5 +1,7 @@
 // pages/home-music/index.js
-import { getBanners } from '../../service/api_music'
+import { rankingStore, rankingMap } from '../../store/index'
+
+import { getBanners, getSongMenu } from '../../service/api_music'
 import queryRect from '../../utils/query-rect'
 import throttle from '../../utils/throttle'
 
@@ -8,18 +10,43 @@ const throttleQueryRect = throttle(queryRect, 1000)
 Page({
   data: {
     swiperHeight: 0,
-    banners: []
+    banners: [],
+    hotSongMenu: [],
+    recommendSongMenu: [],
+    recommendSongs: [],
+    rankings: { 0: {}, 2: {}, 3: {} }
   },
 
   onLoad: function (options) {
     // 获取页面数据
     this.getPageData()
+
+    // 发起共享数据的请求
+    rankingStore.dispatch("getRankingDataAction")
+
+    // 从store获取共享的数据
+    rankingStore.onState("hotRanking", (res) => {
+      if (!res.tracks) return
+      const recommendSongs = res.tracks.slice(0, 6)
+      this.setData({ recommendSongs })
+    })
+    rankingStore.onState("newRanking", this.getRankingHandler(0))
+    rankingStore.onState("originRanking", this.getRankingHandler(2))
+    rankingStore.onState("upRanking", this.getRankingHandler(3))
   },
 
   // 网络请求
   getPageData: function() {
     getBanners().then(res => {
       this.setData({ banners: res.banners })
+    })
+
+    getSongMenu().then(res => {
+      this.setData({ hotSongMenu: res.playlists })
+    })
+
+    getSongMenu("华语").then(res => {
+      this.setData({ recommendSongMenu: res.playlists })
     })
   },
 
@@ -38,7 +65,52 @@ Page({
     })
   },
 
-  onUnload: function () {
+  handleMoreClick: function() {
+    this.navigateToDetailSongsPage("hotRanking")
+  },
 
-  }
+  handleRankingItemClick: function(event) {
+    const idx = event.currentTarget.dataset.idx
+    const rankingName = rankingMap[idx]
+    this.navigateToDetailSongsPage(rankingName)
+  },
+
+  navigateToDetailSongsPage: function(rankingName) {
+    wx.navigateTo({
+      url: `/pages/detail-songs/index?ranking=${rankingName}&type=rank`,
+    })
+  },
+
+  onUnload: function () {
+    // rankingStore.offState("newRanking", this.getNewRankingHandler)
+  },
+
+  getRankingHandler: function(idx) {
+    return (res) => {
+      if (Object.keys(res).length === 0) return
+      const name = res.name
+      const coverImgUrl = res.coverImgUrl
+      const playCount = res.playCount
+      const songList = res.tracks.slice(0, 3)
+      const rankingObj = {name, coverImgUrl, playCount, songList}
+      const newRankings = { ...this.data.rankings, [idx]: rankingObj}
+      this.setData({ 
+        rankings: newRankings
+      })
+    }
+  },
+
+
+  // getUpRankingHandler: function(res) {
+  //   if (Object.keys(res).length === 0) return
+  //   const name = res.name
+  //   const coverImgUrl = res.coverImgUrl
+  //   const songList = res.tracks.slice(0, 3)
+  //   const rankingObj = {name, coverImgUrl, songList}
+  //   const originRankings = [...this.data.rankings]
+  //   originRankings.push(rankingObj)
+  //   this.setData({ 
+  //     rankings: originRankings
+  //   })
+  // }
 })
--- service/api_music.js
+++ service/api_music.js
@@ -5,3 +5,24 @@ export function getBanners() {
     type: 2
   })
 }
+
+export function getRankings(idx) {
+  return hyRequest.get("/top/list", {
+    idx
+  })
+}
+
+// cat -> category 类别
+export function getSongMenu(cat="全部", limit=6, offset=0) {
+  return hyRequest.get("/top/playlist", {
+    cat,
+    limit,
+    offset
+  })
+}
+
+export function getSongMenuDetail(id) {
+  return hyRequest.get("/playlist/detail/dynamic", {
+    id
+  })
+}
